3e2e35aabf413def4b5d7f0e7dc87dfdeb4acf57
Gabriel Wolf <115802771+gDABLIUolf@users.noreply.github.com>
2025-05-11 19:23:30 -0300

    Build: adicionando nodemon e configurando prisma

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express from 'express';
-import { PrismaClient } from '@prisma/client';
+import express from "express";
+import { PrismaClient } from "./generated/prisma";
 
 const app = express();
 const prisma = new PrismaClient();
 
-app.get('/', async (req, res) => {
+app.get("/", async (req: any, res: any) => {
   const users = await prisma.user.findMany();
   res.json(users);
 });
 
 app.listen(3000, () => {
-  console.log('Servidor rodando em http://localhost:3000');
+  console.log("Servidor rodando em http://localhost:3000");
 });
